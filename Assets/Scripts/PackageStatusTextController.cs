e543de9930981b01a23d77972d9733964038ecee
Andrei <andreibvmw@gmail.com>
2022-08-19 19:01:31 +0300

    Updated package status text to use DoTween

--- Assets/Scripts/PackageStatusTextController.cs
+++ Assets/Scripts/PackageStatusTextController.cs
@@ -1,32 +1,34 @@
-using System;
-using System.Threading.Tasks;
-using TMPro;
-using UnityEngine;
-
 /**
  * Created Date: 11/26/2021
  * Author: Andrei-Florin Ciobanu
  * 
  * Copyright (c) 2021 Andrei-Florin Ciobanu. All rights reserved. 
  */
+
+using System;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+using DG.Tweening;
+using TMPro;
+
 public class PackageStatusTextController : MonoBehaviour {
 	public enum PackageStatus {
 		Picked = 0,
 		Delivered = 1
 	}
-
-	private Animator _animator;
+	
 	private TMP_Text _text;
 
 	private void Awake() {
-		this._animator = this.GetComponent<Animator>();
 		this._text = this.GetComponent<TMP_Text>();
 	}
 
 	public async void ShowPackageStatus(PackageStatus status) {
 		this._text.text = status == PackageStatus.Picked ? "Package picked!" : "Package delivered!";
-		this._animator.Play("PackageTextAnimationFadeIn");
+		this._text.transform.DOScale(Vector3.one, 0.4f).SetEase(Ease.OutBack);
 		await Task.Delay(TimeSpan.FromSeconds(1f));
-		this._animator.Play("PackageTextAnimationFadeOut");
+		this._text.transform.DOScale(Vector3.zero, 0.4f);
 	}
 }

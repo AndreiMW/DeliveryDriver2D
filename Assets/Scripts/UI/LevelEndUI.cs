ee1ce79fce3b2e58c5796019416188427b767467
Andrei-Florin Ciobanu <andrei1805florin@gmail.com>
2022-08-29 11:38:59 +0300

    Added scripts to handle level end ui

---
+++ Assets/Scripts/UI/LevelEndUI.cs
@@ -0,0 +1,35 @@
+﻿/**
+ * Created Date: 8/29/2022
+ * Author: Andrei-Florin Ciobanu
+ * 
+ * Copyright (c) 2022 Andrei-Florin Ciobanu. All rights reserved. 
+ */
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+	public class LevelEndUI : MonoBehaviour {
+		[SerializeField]
+		private Button _button;
+		
+		#region Lifecycle
+
+		private void Awake() {
+			this._button.onClick.AddListener(this.HandleButtonAction);
+		}
+
+		#endregion
+		
+		#region Protected
+
+		/// <summary>
+		/// Handle the action for the continue/restart button.
+		/// </summary>
+		protected virtual void HandleButtonAction() {
+			LevelManager.Instance.LoadLevel();
+		}
+		
+		#endregion
+	}
+}
---
+++ Assets/Scripts/UI/LevelSuccessUI.cs
@@ -0,0 +1,20 @@
+﻿/**
+ * Created Date: 8/29/2022
+ * Author: Andrei-Florin Ciobanu
+ * 
+ * Copyright (c) 2022 Andrei-Florin Ciobanu. All rights reserved. 
+ */
+
+namespace UI {
+	public class LevelSuccessUI : LevelEndUI {
+		
+		#region Protected
+
+		protected override void HandleButtonAction() {
+			LevelManager.Instance.IncrementLevelIndex();
+			base.HandleButtonAction();
+		}
+
+		#endregion
+	}
+}

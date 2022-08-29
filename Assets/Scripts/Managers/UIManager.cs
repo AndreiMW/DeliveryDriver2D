74b91124d4399b354eab8b4cb6d3748f70c74de3
Andrei-Florin Ciobanu <andrei1805florin@gmail.com>
2022-08-29 12:27:01 +0300

    Added UIManager

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,35 @@
+﻿/**
+ * Created Date: 8/29/2022
+ * Author: Andrei-Florin Ciobanu
+ * 
+ * Copyright (c) 2022 Andrei-Florin Ciobanu. All rights reserved. 
+ */
+
+using UI;
+using UnityEngine;
+
+namespace Managers {
+	public class UIManager : UnitySingleton<UIManager> {
+		[SerializeField]
+		private LevelFailedUI _levelFailedUI;
+
+		[SerializeField]
+		private LevelSuccessUI _levelSuccessUI;
+
+		#region Public
+
+		/// <summary>
+		/// Display level end UI.
+		/// </summary>
+		/// <param name="success">If true, display level succes, else level failed.</param>
+		public void ShowLevelEndUI(bool success) {
+			if (success) {
+				this._levelSuccessUI.Show();
+			} else {
+				this._levelFailedUI.Show();
+			}
+		}
+		
+		#endregion
+	}
+}
